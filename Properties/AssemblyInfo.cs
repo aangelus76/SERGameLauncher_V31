172a4781ce985ec1a5af6ac79ef5c3904fbb0c7e
Angelus76 <aangelus76@gmail.com>
2025-05-06 07:06:32 +0200

    Version Update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,39 +3,24 @@ using System.Resources;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// Les informations générales relatives à un assembly dépendent de
-// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
-// associées à un assembly.
+// Les informations générales
 [assembly: AssemblyTitle("SER-Games Launcher v3.1")]
 [assembly: AssemblyDescription("Lanceur centralisé pour différentes plateformes de jeux")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Saint-Étienne-du-Rouvray")]
 [assembly: AssemblyProduct("SERGamesLauncher_v3.1")]
-[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyCopyright("SER-76 © 2025")]
 [assembly: AssemblyTrademark("Angelus76")]
 [assembly: AssemblyCulture("")]
 
-// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
-// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
-// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
 [assembly: ComVisible(true)]
 
-// Pour commencer à générer des applications localisables, définissez
-// <UICulture>CultureUtiliséePourCoder</UICulture> dans votre fichier .csproj
-// dans <PropertyGroup>.  Par exemple, si vous utilisez le français
-// dans vos fichiers sources, définissez <UICulture> à fr-FR.
 [assembly: NeutralResourcesLanguage("fr-FR")]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
 [assembly: Guid("bbc7a46f-dbae-4a30-8ba6-3893745d4908")]
 
-// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
-//
-//      Version principale
-//      Version secondaire
-//      Numéro de build
-//      Révision
-//
-[assembly: AssemblyVersion("3.1.0.0")]           // Version produit : 3.1.0.0 (ce qui s'affiche dans les propriétés Windows)
+// Les informations de version
+[assembly: AssemblyVersion("3.1.0.0")]
 [assembly: AssemblyFileVersion("3.1")]  
-[assembly: AssemblyInformationalVersion("W19-25.A1")] // Semaine, Année, Jour  (A -> Lundi, B -> Mardi, C -> Mercredi ...), N° de modification 
+[assembly: AssemblyInformationalVersion("W19-25.B2")] // Semaine, Année, Jour  (A -> Lundi, B -> Mardi, C -> Mercredi ...), N° de modification 
